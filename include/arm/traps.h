c2304bb7d1a4bdd50a86b3776439eb08cc3988d8
Huang Shijie <shijie.huang@arm.com>
2017-07-13 02:06:21 +0000

    arm64: add exception support
    This patch adds the exception support for arm64:
        .0) Add the new pt_regs{} for arm64.
        .1) Add stack macros: trap_entry/trap_exit
        .2) setup the vector table
    
    Change-Id: I61d86ed4516443bf8baf4ef0b13687f9cb2047fd
    Jira: ENTOS-247
    Signed-off-by: Huang Shijie <shijie.huang@arm.com>

--- include/arm/traps.h
+++ include/arm/traps.h
@@ -1,6 +1,7 @@
 #ifndef _TRAPS_H_
 #define _TRAPS_H_
 
+#if defined(__arm__)
 struct pt_regs {
     unsigned long r0;
     unsigned long r1;
@@ -16,5 +17,18 @@ struct pt_regs {
     unsigned long r11;
     unsigned long r12;
 };
+#elif defined(__aarch64__)
+struct pt_regs {
+    /* From x0 ~ x29 */
+    uint64_t x[30];
+    union {
+        uint64_t x30;
+        uint64_t lr;
+    };
+    uint64_t sp;
+    uint64_t pc;
+    uint64_t pstate;
+};
+#endif
 
 #endif
