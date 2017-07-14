f8028a3cdadd51eabe002c5e5f0ca4f08201f21d
Huang Shijie <shijie.huang@arm.com>
2017-07-14 08:27:27 +0000

    arm64: add the basic helpers for arm64
    This patch adds the basic helpers in header os.h for arm32 and arm64:
        0.) mb/rmb/wmb
        1.) local_irq_disable/local_irq_enable
        2.) local_irq_save/local_irq_restore/local_save_flags
        3.) __ffs
    
    This patch refers to Chen Baozi's patch:
         "Initial codes for arm64"
    
    Change-Id: Id9025f016968a815029dfba7ee275d9f757974f0
    Jira: ENTOS-247
    Signed-off-by: Huang Shijie <shijie.huang@arm.com>

---
+++ include/arm/arm64/os.h
@@ -0,0 +1,35 @@
+#ifndef _ARM64_OS_H_
+#define _ARM64_OS_H_
+
+static inline void local_irq_disable(void)
+{
+    __asm__ __volatile__("msr daifset, #2": : :"memory");
+}
+
+static inline void local_irq_enable(void)
+{
+    __asm__ __volatile__("msr daifclr, #2": : :"memory");
+}
+
+#define local_irq_save(x) { \
+    __asm__ __volatile__("mrs %0, daif; msr daifset, #2":"=r"(x)::"memory"); \
+}
+
+#define local_irq_restore(x) { \
+    __asm__ __volatile__("msr daif, %0": :"r"(x):"memory"); \
+}
+
+#define local_save_flags(x) { \
+    __asm__ __volatile__("mrs %0, daif":"=r"(x): :"memory"); \
+}
+
+#define isb()           __asm__ __volatile("isb" : : : "memory")
+
+#define dmb(opt)        __asm__ __volatile("dmb " #opt : : : "memory")
+#define dsb(opt)        __asm__ __volatile("dsb " #opt : : : "memory")
+
+#define mb()            dmb(sy) /* Full system memory barrier all */
+#define wmb()           dmb(st) /* Full system memory barrier store */
+#define rmb()           dmb(ld) /* Full system memory barrier load */
+
+#endif
