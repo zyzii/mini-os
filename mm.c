38f5615fb3924a52eac80ba7cebed6c5adad483f
Huang Shijie <shijie.huang@arm.com>
2017-07-14 09:37:16 +0000

    mini-os: remove the e820 from common code
    The e820 is x86 specific. This patch adds a new
    function arch_check_mem_block() and rename e820_entris to mem_blocks.
    
    Different archs implements the mem_blocks and arch_check_mem_block.
    By this way, we remove the e820 code from the common code.
    
    Signed-off-by: Huang Shijie <shijie.huang@arm.com>

--- mm.c
+++ mm.c
@@ -43,7 +43,6 @@
 #include <mini-os/types.h>
 #include <mini-os/lib.h>
 #include <mini-os/xmalloc.h>
-#include <mini-os/e820.h>
 
 /*********************
  * ALLOCATION BITMAP
@@ -175,15 +174,11 @@ static void init_page_allocator(unsigned long min, unsigned long max)
     /* All allocated by default. */
     memset(mm_alloc_bitmap, ~0, mm_alloc_bitmap_size);
 
-    for ( m = 0; m < e820_entries; m++ )
+    for ( m = 0; m < mem_blocks; m++ )
     {
-        if ( e820_map[m].type != E820_RAM )
+        if (arch_check_mem_block(m, &r_min, &r_max))
             continue;
-        if ( e820_map[m].addr + e820_map[m].size >= ULONG_MAX )
-            BUG();
 
-        r_min = e820_map[m].addr;
-        r_max = r_min + e820_map[m].size;
         if ( r_max <= min || r_min >= max )
             continue;
         if ( r_min < min )
