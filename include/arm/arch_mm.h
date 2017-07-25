c670d882e780b780ba0bffa506d008497fe47cc4
Huang Shijie <shijie.huang@arm.com>
2017-07-25 10:15:21 +0000

    arm64: change physical_address_offset to paddr_t
    The "uint32_t" is not enough for arm64 code,
    This patch changes it to paddr_t, and also fixes the print format.
    
    Signed-off-by: Huang Shijie <shijie.huang@arm.com>

--- include/arm/arch_mm.h
+++ include/arm/arch_mm.h
@@ -2,11 +2,14 @@
 #define _ARCH_MM_H_
 
 typedef uint64_t paddr_t;
+#define PRIpaddr "lx"
 
 extern char _text, _etext, _erodata, _edata, _end, __bss_start;
 extern int _boot_stack[];
 extern int _boot_stack_end[];
-extern uint32_t physical_address_offset;	/* Add this to a virtual address to get the physical address (wraps at 4GB) */
+
+/* Add this to a virtual address to get the physical address */
+extern paddr_t physical_address_offset;
 
 #include <page_def.h>
 
